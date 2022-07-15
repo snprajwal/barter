be4067f4743944efa6dc9afd56dda24f82526a79
Prajwal S N <prajwalnadig21@gmail.com>
2022-07-15 20:02:11 +0530

    feat: dockerise, add validation
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>

--- backend/src/modules/db.js
+++ backend/src/modules/db.js
@@ -1,9 +1,9 @@
-const { Pool } = require('pg')
+const {Pool} = require('pg')
 
 const pool = new Pool({
 	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT
 })
@@ -11,23 +11,23 @@ const pool = new Pool({
 const getPass = async (email) => {
 	try {
 		const res = await pool.query('SELECT password FROM users WHERE email = $1', [email])
-		if (!res.rows[0]) { return null }
+		if (!res.rows[0]) {return null}
 		return res.rows[0].password
-	} catch (err) { console.log(err) }
+	} catch (err) {console.log(err)}
 }
 
 const createUser = async (name, email, phone, pass) => {
 	try {
 		await pool.query('INSERT INTO users (name, email, phone, password) VALUES ($1, $2, $3, $4)', [name, email, phone, pass])
 		return console.log('New user created')
-	} catch (err) { console.log(err) }
+	} catch (err) {console.log(err)}
 }
 
 const createProduct = async (name, quantity, price) => {
 	try {
 		await pool.query('INSERT INTO products (name, quantity, price) VALUES ($1, $2, $3)', [name, quantity, price])
 		return console.log('New product created')
-	} catch (err) { console.log(err) }
+	} catch (err) {console.log(err)}
 }
 
-module.exports = { getPass, createUser, createProduct }
+module.exports = {getPass, createUser, createProduct}
--- backend/src/routes/signup.js
+++ backend/src/routes/signup.js
@@ -1,16 +1,31 @@
-const { getPass, createUser } = require('../modules/db')
+const {getPass, createUser} = require('../modules/db')
 const bcrypt = require('bcrypt')
+const {check, validationResult} = require('express-validator')
 const router = require('express').Router()
 
-router.post('/', async (req, res) => {
-	const { name, phone, email, password } = req.body
+router.post('/', [
+	check('email').isEmail(),
+	check('password').isLength({min: 1}),
+	check('name').isLength({min: 1})
+], async (req, res) => {
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		return res.status(400).json({'error': errors.array()})
+	}
+
+	const {name, phone, email, password} = req.body
 	try {
 		let pass = await getPass(email)
-		if (pass) { return res.status(500).send('User already exists, try logging in') }
+		if (pass) {
+			return res.status(500).json({
+				'error': 'ERR_USER_EXISTS',
+				'msg': 'User already exists, try logging in'
+			})
+		}
 		pass = await bcrypt.hash(password, 5)
 		await createUser(name, email, phone, pass)
-		return res.status(201).send('New user created')
-	} catch (err) { console.log(err) }
+		return res.status(201).json({'msg': 'New user created'})
+	} catch (err) {console.log(err)}
 })
 
 module.exports = router
